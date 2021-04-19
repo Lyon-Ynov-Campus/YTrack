b228b597b2767194d42e7b076b3b4cc62bf38dd9
VRIAMON1 <v.riamon@ynov.com>
2021-04-19 13:53:51 +0200

    modified room-is-clean subject and test

--- js/tests/room-is-clean_test.js
+++ js/tests/room-is-clean_test.js
@@ -3,31 +3,31 @@ export const tests = []
 const t = (f) => tests.push(f)
 
 //Test waitAndExecute before 1s should do nothing
-t(async ({eq, ctx}) => {
-    const toto = []
-    const promise = new Promise((resolve) => {
-        waitAndExecute((...args) => {
-            toto.push(...args)  
-            resolve("executed")
-        })("a", "b")
-    })
-    const res = await Promise.race([ctx.delayed(500)("timeout"), promise])
-    return eq(res[0], "timeout") && eq(toto.length, 0)
-})
+// t(async ({eq, ctx}) => {
+//     const toto = []
+//     const promise = new Promise((resolve) => {
+//         waitAndExecute((...args) => {
+//             toto.push(...args)  
+//             resolve("executed")
+//         })("a", "b")
+//     })
+//     const res = await Promise.race([ctx.delayed(500)("timeout"), promise])
+//     return eq(res[0], "timeout") && eq(toto.length, 0)
+// })
 
-// Test waitAndExecute works after 1s
-t(async ({eq, ctx}) => {
-    const toto = []
-    const promise = new Promise((resolve) => {
-        waitAndExecute((...args) => {
-            toto.push(...args)  
-            resolve("executed")
-        })("a", "b")
-    })
-   const res = await Promise.race([ctx.delayed(1100)("timeout"), promise])
+// // Test waitAndExecute works after 1s
+// t(async ({eq, ctx}) => {
+//     const toto = []
+//     const promise = new Promise((resolve) => {
+//         waitAndExecute((...args) => {
+//             toto.push(...args)  
+//             resolve("executed")
+//         })("a", "b")
+//     })
+//    const res = await Promise.race([ctx.delayed(1100)("timeout"), promise])
    
-   return eq(res, "executed") && eq(JSON.stringify(toto), JSON.stringify(["a", "b"])) 
-})
+//    return eq(res, "executed") && eq(JSON.stringify(toto), JSON.stringify(["a", "b"])) 
+// })
 
 // Test isMyRoomClean true before 1s
 t(async ({eq, ctx}) => {
